be5853a61a4cc5d2d96b5e54010199ae27d8e70d
Lizzeth Marian Guadarrama <152321311+Lizgv02@users.noreply.github.com>
2023-11-28 16:09:41 -0600

    Add files via upload

---
+++ Envios/envioAgregar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JuVa.Models;
+using JuVa.Utilerias;
+
+namespace JuVa.Views.Titulaciones
+{
+    public partial class envioAgregar : Form
+    {
+        private readonly int ID = 20;
+        private UsuariosModel modelo;
+        public envioAgregar(UsuariosModel modelo)
+        {
+            InitializeComponent();
+            Querys q = new Querys();
+            if (!Querys.tienePermiso(modelo.Tipo, ID))
+            {
+                MessageBox.Show(Properties.Resources.sinPermiso2);
+                this.Close();
+            }
+        }
+    }
+}
